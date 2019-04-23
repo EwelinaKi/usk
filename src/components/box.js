25caec6a720741cb3f647d00cb1cbe6aac1492d5
Ewelina Kijanowska <kijanowska@gmail.com>
2019-04-23 16:29:53 +0200

    box section new css outlines

--- src/components/box.js
+++ src/components/box.js
@@ -6,7 +6,7 @@ import boxStyles from "../styles/box.module.css"
 
 export default (props) => {
   return (
-  <div className={boxStyles.box}>
+    <div>
     <Row>
       <Col md={6} sm={12} className={boxStyles.recommendations}>
         <Recommendations recommendations={props.recommendations.nodes}/>
--- src/components/recommendations.js
+++ src/components/recommendations.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { Card, Carousel } from "react-bootstrap"
+import recommendationStyles from "../styles/recommendations.module.css"
 
 export default (props) => (
   <Carousel>
     {props.recommendations.map(recommendation =>
       (
         <Carousel.Item key={recommendation.id}>
-          <Card style={{ width: "20rem" }}>
-            {/*<Card.Img variant="top" src={}/>*/}
+          <Card style={{ width: "20rem" }} className={recommendationStyles.cardAll}>
+            <Card.Img variant="top" src={"images/" + recommendation.image} className={recommendationStyles.cardImg}/>
             <Card.Body>
               <Card.Title>{recommendation.name}</Card.Title>
               <span role="img" aria-label="rate">{recommendation.rate}</span>
-              <Card.Text>
+              <Card.Text className={recommendationStyles.cardText}>
                 {recommendation.opinion}
               </Card.Text>
             </Card.Body>
