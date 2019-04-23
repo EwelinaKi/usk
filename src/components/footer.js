8ccfa445aa72d2da4f9a29c0c35fe1f361cee19e
Ewelina Kijanowska <kijanowska@gmail.com>
2019-04-23 19:09:57 +0200

    footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,34 @@
 import React from "react"
 import { Col, Row } from "react-bootstrap"
-import styles from "../styles/footer.module.css"
+import footerStyles from "../styles/footer.module.css"
 
 export default () => (
-  <div className={styles.box}>
+  <div className={footerStyles.box}>
     <Row>
       <Col>
         <h5>My</h5>
-        <p>github</p>
-        <p>linkedIn</p>
+        <a href="http://github.com" className={footerStyles.link}>
+          <img src="svg/github.svg" alt="github logo" className={footerStyles.svgLogo}/>
+          Github
+        </a>
+        <br/>
+        <a href="http://linkedin.com" className={footerStyles.link}>
+          <img src="svg/github.svg" alt="github logo" className={footerStyles.svgLogo}/>
+          linkedIn
+        </a>
       </Col>
       <Col>
-        <h5>Credentials</h5>
-        <p>github</p>
-        <p>linkedIn</p>
+        <h5>Attributions</h5>
+        Awards free Vectors by <a rel="nofollow" href="https://www.vecteezy.com"
+                                  className={footerStyles.link}>Vecteezy!</a>
+        <br/>
+        Plane photo by <a rel="nofollow" href="https://www.pexels.com/photo/photo-of-airplane-with-smoke-trail-2088203/"
+                          className={footerStyles.link}>eberhard grossgasteiger</a>
+        <br/>
+        Keyboard photo by <a rel="nofollow"
+                             href="https://www.pexels.com/photo/apple-background-desk-electronics-399161/"
+                             className={footerStyles.link}>
+        Lum3n.com</a>
       </Col>
     </Row>
   </div>
